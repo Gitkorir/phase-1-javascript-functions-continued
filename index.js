530a14584741f09f26877c009570499adc6c1b8f
Arnold Korir <arnoldkorir201@gmail.com>
2025-03-06 14:38:03 +0300

    create the inde.js structure

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // code your solution here
+// index.js
+
+// Function for Saturday fun activity
+function saturdayFun(activity = "roller-skate") {
+  return `This Saturday, I want to ${activity}!`;
+}
+
+// Function for Monday work activity
+const mondayWork = function (activity = "go to the office") {
+  return `This Monday, I will ${activity}.`;
+};
+
+// Function to wrap an adjective with the specified delimiter
+function wrapAdjective(delimiter = "*") {
+  return function (adjective) {
+    return `You are ${delimiter}${adjective}${delimiter}!`;
+  };
+}
+
+module.exports = {
+  saturdayFun,
+  mondayWork,
+  wrapAdjective,
+};
